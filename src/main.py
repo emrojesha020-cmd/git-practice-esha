ac57ddb451858020603deabc23ba44d8fed36389
Esha <emrojesha020@gmail.com>
2026-05-02 00:02:36 +0600

    added basic error handling

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 from utils import add, subtract, multiplication
+
 print('Esha')
 print('1/04/2026')
 
-a = 5
-b = 3
+try:
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
 
-print("Addition:", add(a, b))
-print("Subtraction:", subtract(a, b))
-print("Multiplication:", multiplication(a, b))
+    print("Addition:", add(a, b))
+    print("Subtraction:", subtract(a, b))
+    print("Multiplication:", multiplication(a, b))
+
+except ValueError:
+    print("Error: enter valid numbers")
